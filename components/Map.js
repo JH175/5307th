6159c72e4efc32af8df98be90f434718d542a1fc
JH175 <JH175@protonmail.com>
2022-08-01 20:46:42 -0400

    Add protected API to Map.js

--- components/Map.js
+++ components/Map.js
@@ -2,15 +2,15 @@ import React, { useRef, useEffect, useState } from 'react';
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
-mapboxgl.accessToken =
-  'pk.eyJ1IjoiamgxNzUiLCJhIjoiY2t3eTlwbnNrMGozbTJvcXZmYzIwaWkyciJ9.Ez4_u57k7BFDx2i4CkM2Dw';
-
 function Map() {
   const mapContainer = useRef(null);
   const map = useRef(null);
   const [lng, setLng] = useState(94.27);
   const [lat, setLat] = useState(22.91);
   const [zoom, setZoom] = useState(4.5);
+
+  mapboxgl.accessToken =
+    'pk.eyJ1IjoiamgxNzUiLCJhIjoiY2w2YmdqaHR5MTZleTNrcWhwcDNiNThiciJ9.NNowZ67HCZ46fJRtMphXEw';
 
   useEffect(() => {
     if (map.current) return; // initialize map only once
